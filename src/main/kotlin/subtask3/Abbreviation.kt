ee71ee1ed8a43ea71031bdc376cf90358eec3a1d
Denis Drozdov <drazdou.work@gmail.com>
2020-04-09 18:46:24 +0300

    feat: add solution subtask3 Abbreviation

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -4,6 +4,18 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        var bMod = b
+        var result = mutableListOf<Char>()
+
+        for (char in a.toUpperCase()){
+            for (i in bMod){
+                if (char == i){
+                    result.add(char)
+                    bMod = bMod.substringAfter(char)
+                }
+            }
+        }
+
+        return  if (b == result.joinToString ( "" )) "YES" else "NO"
     }
 }
