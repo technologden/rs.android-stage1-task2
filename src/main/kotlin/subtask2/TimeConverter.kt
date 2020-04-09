f5765bf111ec442e1bb8c5dc39c538bf239a6b33
Denis Drozdov <drazdou.work@gmail.com>
2020-04-09 18:45:08 +0300

    feat: add solution subtask2 TimeConverter

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -1,9 +1,44 @@
 package subtask2
 
+import java.lang.NumberFormatException
+import java.time.DateTimeException
+import java.time.LocalTime
+
 class TimeConverter {
 
     // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+        try {
+            val time = LocalTime.of(hour.toInt(), minute.toInt())
+            return when(time.minute){
+                0 -> "${numToText(time.hour)} o' clock"
+                1 -> "${numToText(time.minute)} minute past ${numToText(time.hour)}"
+                in (2..29) -> {
+                    if (time.minute == 15)
+                        "quarter past ${numToText(time.hour)}"
+                    else "${numToText(time.minute)} minutes past ${numToText(time.hour)}"
+                }
+                30 -> "half past ${numToText(time.hour)}"
+                in (31..58) -> {
+                    if (time.minute == 45)
+                        "quarter to ${numToText(time.hour+1)}"
+                    else "${numToText(60 - time.minute)} minutes to ${numToText(time.hour + 1)}"
+                }
+                59 -> "${numToText(60 - time.minute)} minute to ${numToText(time.hour + 1)}"
+                else -> ""
+            }
+        } catch (e: DateTimeException){
+            return ""
+        } catch (e: NumberFormatException){
+            return ""
+        }
     }
+}
+
+fun numToText(number: Int): String{
+    val array = arrayOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty",
+        "twenty one", "twenty two", "twenty three", "twenty four", "twenty five", "twenty six", "twenty seven",
+        "twenty eight", "twenty nine")
+    return array[number-1]
 }
